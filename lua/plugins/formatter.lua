f8ed6f918b21e864bb7a76576c62f83d6f086275
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-06-14 12:17:01 +0200

    Added formatter for plaintex

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -69,6 +69,9 @@ return {
 				tex = {
 					require('formatter.filetypes.latex').latexindent,
 				},
+				plaintex = {
+					require('formatter.filetypes.latex').latexindent,
+				},
 				javascript = { prettier },
 				javascriptreact = { prettier },
 				typescript = { prettier },
