2a6a553b2fba35e307233b8daf2e17e880701644
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-08-04 12:46:58 +0200

    Disable focus for certain buffer and file types

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -13,3 +13,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 		}
 	end,
 })
+
+local ignore_filetypes = { 'neo-tree' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+vim.api.nvim_create_autocmd('WinEnter', {
+	group = augroup,
+	callback = function(_)
+		if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+			vim.b.focus_disable = true
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+	group = augroup,
+	callback = function(_)
+		if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+			vim.b.focus_disable = true
+		end
+	end,
+})
