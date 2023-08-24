a4cf6e86abc9713ee04e71c4135979ca6e54c483
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-08-24 11:59:47 +0200

    Forgot to stage files lol

---
+++ lua/plugins/linter.lua
@@ -0,0 +1,16 @@
+return {
+	'mfussenegger/nvim-lint',
+	ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'c', 'cpp', 'cmake' },
+	opts = {
+		typescript = { 'eslint_d' },
+		typescriptreact = { 'eslint_d' },
+		javascript = { 'eslint_d' },
+		javascriptreact = { 'eslint_d' },
+		c = { 'clangtidy' },
+		cpp = { 'clangtidy' },
+		cmake = { 'cmakelint' },
+	},
+	config = function(_, opts)
+		require('lint').linters_by_ft = opts
+	end,
+}
---
+++ lua/required-tools.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+M.lsps = {
+	'clangd',
+	'texlab',
+	'lua_ls',
+	'tsserver',
+	'pyright',
+	'marksman',
+	'cmake',
+	'vimls',
+	'jdtls',
+}
+
+M.formatter = {
+	'black',
+	'clang-format',
+	'latexindent',
+	'prettier',
+	'stylua',
+}
+
+M.linter = {
+	'cmakelint',
+	'eslint_d',
+}
+
+M.parsers = {
+	'c',
+	'cpp',
+	'cmake',
+	'make',
+	'comment',
+	'llvm',
+	'vim',
+	'vimdoc',
+	'vimdoc',
+	'lua',
+	'query',
+	'kdl',
+	'javascript',
+	'java',
+	'haskell',
+	'latex',
+	'bibtex',
+	'python',
+	'markdown',
+	'gitcommit',
+	'gitignore',
+	'regex',
+}
+
+return M
