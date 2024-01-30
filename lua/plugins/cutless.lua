a9022ac726f17d2046dbacdd5eab3a95e3af2244
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-01-30 20:16:29 +0100

    Revert "Switch from vim-cutlass to karen-yank"
    This reverts commit c9b0e70c5227dec5593085c5e89fb45770ff218f.

--- lua/plugins/cutless.lua
+++ lua/plugins/cutless.lua
@@ -1,14 +1,5 @@
 return {
-	'tenxsoydev/karen-yank.nvim',
-	keys = {
-		{ 'm', function() require('karen-yank.actions').cut('d') end, },
-		{ 'M', function() require('karen-yank.actions').cut('D') end, },
-		{ 'mm', 'dd', },
-		{'d'}, {'md'}, {'D'}, {'mD'}, {'c'}, {'mc'}, {'y'}, {'yy'}, {'Y'}, {'p'}, {'P'}
-	},
-	opts = {
-		karen = 'y',
-		invert = false,
-		disable = { 's', 'S' },
-	},
+	'LukasPietzschmann/vim-cutlass',
+	event = 'VeryLazy',
+	dev = true,
 }
