e2b803c52b9586886809360368005e8c304d569c
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-01-30 21:23:40 +0100

    Updated copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,6 +5,17 @@ return {
 	build = ':Copilot auth',
 	config = function(_, opts)
 		require('copilot').setup(opts)
+		local suggestion = require 'copilot.suggestion'
+		vim.keymap.set('i', '<Esc>', function()
+			if suggestion.is_visible() then
+				suggestion.dismiss()
+			end
+			return '<Esc>'
+		end, {
+			desc = '[copilot] dismiss suggestion',
+			expr = true,
+			silent = true,
+		})
 	end,
 	opts = {
 		panel = { enabled = false },
@@ -12,10 +23,21 @@ return {
 			enabled = true,
 			auto_trigger = true,
 			keymap = {
-				accept = '<Tab>', -- Also mapped in cmp.lua
-				dismiss = '<Esc>',
+				accept = false, -- handled by cmp
+				dismiss = false,
+				accept_word = false,
+				accept_line = false,
+				next = false,
+				prev = false,
 			},
 		},
 		filetypes = { ['*'] = true },
+		server_opts_overrides = {
+			settings = {
+				advanced = {
+					inlineSuggestCount = 1,
+				},
+			},
+		},
 	},
 }
