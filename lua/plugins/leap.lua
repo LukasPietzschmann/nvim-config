104179aab60cfbe54b99d15df4e71d8590379f51
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-02-17 22:20:18 +0100

    Updated leap config

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,19 +5,9 @@ return {
 		config = function()
 			local leap = require 'leap'
 			leap.add_default_mappings()
-			leap.opts.safe_labels = {}
 			leap.opts.case_sensitive = false
 			leap.opts.equivalence_classes = { ' \t\r\n' }
 			-- leap.opts.labels = { 'w', 'a', 's', 'e', 'd', 'j', 'k', 'l' }
 		end,
-	},
-	{
-		'ggandor/flit.nvim',
-		keys = { 'f', 'F', 't', 'T' },
-		opts = {
-			keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-			multiline = true,
-		},
-		dependencies = { 'ggandor/leap.nvim' },
 	},
 }
