7056e31e14b497debf506a5c26938c703eba6823
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-02-18 15:40:25 +0100

    Tex $$ pair

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,4 +1,21 @@
 return {
 	'windwp/nvim-autopairs',
-	event = 'VeryLazy',
+	event = 'InsertEnter',
+	opts = {
+		check_ts = true,
+		enable_check_bracket_line = false,
+	},
+	config = function(_, opts)
+		require('nvim-autopairs').setup(opts)
+		local Rule = require 'nvim-autopairs.rule'
+		local npairs = require 'nvim-autopairs'
+		local cond = require 'nvim-autopairs.conds'
+
+		npairs.add_rules {
+			Rule('$', '$', { 'tex', 'plaintex', 'latex' })
+				:with_pair(cond.not_before_regex '%%')
+				:with_move(cond.none())
+				:with_cr(cond.none()),
+		}
+	end,
 }
