b127c4323d6af6162d694b6b347b133a90f9483a
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-02-27 21:22:18 +0100

    Cook all ducks at once

--- lua/plugins/duck.lua
+++ lua/plugins/duck.lua
@@ -2,6 +2,6 @@ return {
 	'tamton-aquib/duck.nvim',
 	keys = {
 		{ '<C-q>h', function() require('duck').hatch() end },
-		{ '<C-q>c', function() require('duck').cook() end },
+		{ '<C-q>c', function() require('duck').cook_all() end },
 	},
 }
