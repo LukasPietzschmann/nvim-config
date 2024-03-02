998dc4a72d407ff4aebb5634ba55e6b2e3bff76b
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-03-02 21:43:53 +0100

    Removed telescope-fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -106,14 +106,6 @@ return {
 		dependencies = { 'nvim-lua/plenary.nvim' },
 	},
 	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'make',
-		dependencies = { 'nvim-telescope/telescope.nvim' },
-		config = function()
-			require('telescope').load_extension 'fzf'
-		end,
-	},
-	{
 		'LukasPietzschmann/telescope-tabs',
 		keys = {
 			{ '<A-k>', function() require('telescope-tabs').list_tabs() end },
