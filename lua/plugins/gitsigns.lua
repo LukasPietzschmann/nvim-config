4c70692039fe2d4672735973610fc83e8799e38d
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-06-25 14:39:04 +0200

    Updated gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,16 +3,11 @@ return {
 	event = { 'BufReadPre', 'BufAdd' },
 	opts = {
 		signs = {
-			add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-			change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-			delete = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-			topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-			changedelete = {
-				hl = 'GitSignsChange',
-				text = '│',
-				numhl = 'GitSignsChangeNr',
-				linehl = 'GitSignsChangeLn',
-			},
+			add = { text = '│' },
+			change = { text = '│' },
+			delete = { text = '│' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '│' },
 		},
 		signcolumn = true,
 		numhl = true,
