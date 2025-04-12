97d7e0c21d346d1b752295c5963c58924afd0e51
Lukas Pietzschmann <lukas@pietzschmann.org>
2025-04-13 01:26:00 +0200

    Fixed indenting

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,14 +1,12 @@
 return {
-	{
-		'ggandor/leap.nvim',
-		keys = { 's', 'S', 'x', 'X', 'gs', 'f', 'F' },
-		config = function()
-			local leap = require 'leap'
-			leap.add_default_mappings()
-			leap.opts.case_sensitive = false
-			leap.opts.equivalence_classes = { ' \t\r\n' }
-			leap.opts.safe_labels = ''
-			leap.opts.labels = 'asdfjklqweopmn,.äö'
-		end,
-	},
+	'ggandor/leap.nvim',
+	keys = { 's', 'S', 'x', 'X', 'gs', 'f', 'F' },
+	config = function()
+		local leap = require 'leap'
+		leap.add_default_mappings()
+		leap.opts.case_sensitive = false
+		leap.opts.equivalence_classes = { ' \t\r\n' }
+		leap.opts.safe_labels = ''
+		leap.opts.labels = 'asdfjklqweopmn,.äö'
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,45 @@
 local required_tools = lazy_require 'required-tools'
 
 return {
-	{
-		'nvim-treesitter/nvim-treesitter',
-		lazy = vim.fn.argc(-1) == 0,
-		event = { 'BufReadPre', 'BufAdd' },
-		cmd = 'TSUpdate',
-		build = ':TSUpdate',
-		opts = {
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = false,
-					keymaps = {
-						['if'] = '@function.inner',
-						['af'] = '@function.outer',
-						['ip'] = '@parameter.inner',
-						['ap'] = '@parameter.outer',
-						['as'] = '@statement.outer',
-						['ia'] = '@attribute.outer',
-						['aa'] = '@attribute.outer',
-					},
+	'nvim-treesitter/nvim-treesitter',
+	lazy = vim.fn.argc(-1) == 0,
+	event = { 'BufReadPre', 'BufAdd' },
+	cmd = 'TSUpdate',
+	build = ':TSUpdate',
+	opts = {
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = false,
+				keymaps = {
+					['if'] = '@function.inner',
+					['af'] = '@function.outer',
+					['ip'] = '@parameter.inner',
+					['ap'] = '@parameter.outer',
+					['as'] = '@statement.outer',
+					['ia'] = '@attribute.outer',
+					['aa'] = '@attribute.outer',
 				},
-				--[[ swap = {
-					enable = true,
-					swap_next = {
-						['sp'] = '@parameter.inner',
-					},
-				}, ]]
-				--[[ lsp_interop = {
-					enable = true,
-					border = 'none',
-					peek_definition_code = {
-						['sf'] = '@function.outer',
-						['sc'] = '@class.outer',
-					},
-				}, ]]
 			},
-			ensure_installed = required_tools.parsers,
-			sync_install = false,
-			highlight = { enable = true },
+			--[[ swap = {
+				enable = true,
+				swap_next = {
+					['sp'] = '@parameter.inner',
+				},
+			}, ]]
+			--[[ lsp_interop = {
+				enable = true,
+				border = 'none',
+				peek_definition_code = {
+					['sf'] = '@function.outer',
+					['sc'] = '@class.outer',
+				},
+			}, ]]
 		},
-		main = 'nvim-treesitter.configs',
-		dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+		ensure_installed = required_tools.parsers,
+		sync_install = false,
+		highlight = { enable = true },
 	},
+	main = 'nvim-treesitter.configs',
+	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
 }
